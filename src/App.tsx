03dcc4550ad1a5fa37ef15f79683b724ab4822ea
Hieu Tran <hieu.tran@kuechenheld.de>
2022-04-24 10:46:27 +0700

    feat(stylesheet): add tailwindcss and less

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,11 @@
-import { useState } from 'react'
-
 function App() {
-	const [count, setCount] = useState(0)
-
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<p>Hello Vite + React!</p>
-				<p>
-					<button type='button' onClick={() => setCount((prevCount) => prevCount + 1)}>
-						count is: {count}
-					</button>
-				</p>
-				<p>
-					Edit <code>App.tsx</code> and save to test HMR updates.
-				</p>
-				<p>
-					<a
-						className='App-link'
-						href='https://reactjs.org'
-						target='_blank'
-						rel='noopener noreferrer'
-					>
-						Learn React
-					</a>
-					{' | '}
-					<a
-						className='App-link'
-						href='https://vitejs.dev/guide/features.html'
-						target='_blank'
-						rel='noopener noreferrer'
-					>
-						Vite Docs
-					</a>
-				</p>
-			</header>
+		<div className='h-screen w-screen flex items-center justify-center bg-slate-400'>
+			<div className='h-80 w-36 bg-black flex flex-col items-center justify-around'>
+				<div className='h-20 w-20 rounded-full bg-red-600' />
+				<div className='h-20 w-20 rounded-full bg-yellow-400' />
+				<div className='h-20 w-20 rounded-full bg-green-600' />
+			</div>
 		</div>
 	)
 }
